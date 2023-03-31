b8d7f4e502eee719c7479718b4a955b0d2d9710f
Tomek K <tomasz.kuncio@gmail.com>
2023-03-31 23:23:48 +0200

    add an icon to search input field

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,6 +1,8 @@
 import ContextHook from '../hooks/ContextHook';
+import searchIcon from '../svg/search.svg';
+import searchIconLight from '../svg/searchWhite.svg';
 function Inputs() {
-	const { countryName, setCountryName, filterOption, setFilter } =
+	const { isDarkMode, countryName, setCountryName, filterOption, setFilter } =
 		ContextHook();
 
 	const onChange = (e) => {
@@ -12,14 +14,20 @@ function Inputs() {
 		setFilter(e.target.value);
 	};
 
+	let icon;
+	isDarkMode ? (icon = searchIconLight) : (icon = searchIcon);
 	return (
 		<div className='xl:container xl:mx-auto mx-5 my-8 md:flex md:justify-between md:align-middle'>
-			<input
-				value={countryName}
-				onChange={onChange}
-				aria-label='Country name'
-				className='filter-options-input h-10 block px-5 py-3 w-full md:w-1/2 xl:w-1/3 rounded-md'
-			/>
+			<div className='block relative'>
+				<input
+					value={countryName}
+					placeholder='Search for a country...'
+					onChange={onChange}
+					aria-label='Country name'
+					className='filter-options-input h-10 ps-10 py-3 w-full md:w-1/2 xl:w-1/3 rounded-md'
+				/>
+				<img src={icon} alt='' className='absolute top-1/2 -translate-y-1/2 left-2'/>
+			</div>
 			<select
 				className='filter-options-input h-10 mt-8 md:my-0 px-5 rounded-md'
 				onChange={onChangeOption}>
