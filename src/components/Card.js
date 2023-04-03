c18c6bc6074beaebd23228f44f2b494a6eedb460
Tomek K <tomasz.kuncio@gmail.com>
2023-04-03 18:25:03 +0200

    Add border to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,9 @@ function Card({ flagUrl, id, flagAlt, countryName, population, region, capital, 
 	};
 
 	return (
-		<div id={id} onClick={(e) => clickHandler(e)} className='card w-full rounded-lg overflow-hidden drop-shadow-xl cursor-pointer hover:scale-105 transition-transform'>
+		<div id={id} onClick={(e) => clickHandler(e)} className='card w-full rounded-lg overflow-hidden drop-shadow-xl cursor-pointer hover:scale-105 transition-transform border border-gray-500'>
 			<img src={flagUrl} alt={flagAlt} className='w-full' />
-			<div className='w-full px-5 py-8 '>
+			<div className='w-full px-5 py-8'>
 				<h2 className='text-2xl font-bold leading-loose'>{countryName}</h2>
 				<p>
 					<span className='font-bold leading-relaxed'>Population: </span>{' '}
